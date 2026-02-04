77ba928a14db35a4392898d3e2fd68e9df52289b
lipe <jorgefeliperichtergusmao@gmail.com>
2026-02-03 21:20:13 -0300

    Post system implemented along with CSS changes.

--- principal/forms.py
+++ principal/forms.py
@@ -5,34 +5,38 @@ from principal.models import User
 
 
 class FormLogin(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    button_submit = SubmitField('Login')
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    button_submit = SubmitField("Login")
 
 
 class FormRegistro(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[
-                             DataRequired(), length(min=6), length(max=20)])
-    confirm_password = PasswordField('Confirm Password', validators=[
-                                     DataRequired(), EqualTo('password')])
-    button_submit = SubmitField('Register')
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        "Password", validators=[DataRequired(), length(min=6), length(max=20)]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    button_submit = SubmitField("Register")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError(
-                'Email already registered. Please use a different email.')
+                "Email already registered. Please use a different email."
+            )
 
     def validate_username(self, username):
         user = User.query.filter_by(name=username.data).first()
         if user:
             raise ValidationError(
-                'Username already taken. Please choose a different username.')
+                "Username already taken. Please choose a different username."
+            )
 
 
 class FormPost(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = StringField('Content', validators=[DataRequired()])
-    button_submit = SubmitField('Create Post')
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()])
+    button_submit = SubmitField("Create Post")
--- principal/views.py
+++ principal/views.py
@@ -1,37 +1,36 @@
 from flask import render_template, url_for, redirect
 from principal import app, database, bcrypt
 from flask_login import login_required, logout_user, login_user, current_user
-from principal.forms import FormLogin, FormRegistro
+from principal.forms import FormLogin, FormRegistro, FormPost
 from principal.models import User, Post
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     formlogin = FormLogin()
     if formlogin.validate_on_submit():
         user = User.query.filter_by(email=formlogin.email.data).first()
         if user and bcrypt.check_password_hash(user.password, formlogin.password.data):
             login_user(user, remember=True)
-            return redirect(url_for('perfil', id_user=user.id))
-    return render_template('login.html', form=formlogin)
+            return redirect(url_for("perfil", id_user=user.id))
+    return render_template("login.html", form=formlogin)
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     formregistro = FormRegistro()
     if formregistro.validate_on_submit():
-        password_hashed = bcrypt.generate_password_hash(
-            formregistro.password.data)
+        password_hashed = bcrypt.generate_password_hash(formregistro.password.data)
         user = User(
             name=formregistro.username.data,
             email=formregistro.email.data,
@@ -40,24 +39,42 @@ def register():
         database.session.add(user)
         database.session.commit()
         login_user(user, remember=True)
-        return redirect(url_for('perfil', id_user=user.id))
-    return render_template('register.html', form=formregistro)
+        return redirect(url_for("perfil", id_user=user.id))
+    return render_template("register.html", form=formregistro)
 
 
-@app.route('/perfil/<id_user>')
+@app.route("/perfil/")
+@login_required
+def perfil_user():
+    return redirect(url_for("perfil", id_user=current_user.id))
+
+
+@app.route("/perfil/<id_user>", methods=["GET", "POST"])
 @login_required
 def perfil(id_user):
+    form_post = FormPost()
+    if form_post.validate_on_submit():
+        post = Post(
+            title=form_post.title.data,
+            content=form_post.content.data,
+            id_user=current_user.id,
+        )
+        database.session.add(post)
+        database.session.commit()
+        return redirect(url_for("perfil", id_user=current_user.id))
     if int(id_user) == int(current_user.id):
-        return render_template('perfil.html', username=current_user)
+        return render_template(
+            "perfil.html", username=current_user, form_post=form_post
+        )
     else:
         username = User.query.get(int(id_user))
         if username is None:
-            return redirect(url_for('index'))
-        return render_template('perfil.html', username=username)
+            return redirect(url_for("index"))
+        return render_template("perfil.html", username=username, form_post=form_post)
 
 
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
